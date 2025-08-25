202d6cace47bbcd3ae0e803255f86d9fee62d059
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2025-08-25 21:06:46 +0800

    feat: 移动的文档排序到目标位置的顶部
    https://github.com/TCOTC/move-doc/issues/3

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import {
     Menu,
     Setting,
     fetchPost,
-    getActiveEditor
+    getActiveEditor,
+    fetchSyncPost
 } from "siyuan";
 // import "./index.scss";
 
@@ -65,7 +66,7 @@ export default class PluginSample extends Plugin {
             // 当前文档不存在
             return;
         };
-        let targetId: string;
+        let targetId: string, boxID: string;
 
         // doc: 单个文档；docs: 多个文档 / 文档与笔记本混合；notebook: 单个笔记本
         if (type === "doc") {
@@ -75,6 +76,7 @@ export default class PluginSample extends Plugin {
                 return;
             };
             targetId = targetDocId;
+            boxID = currentDoc.notebookId;
             // TODO跟进: 目前不支持使用异步操作 https://github.com/siyuan-note/siyuan/issues/15676
             // const targetDocInfo = await fetchSyncPost("/api/block/getBlockInfo", { id: targetDocId });
             // if (!targetDocInfo?.data?.path || targetDocInfo.data.path.includes(currentDoc.id)) {
@@ -94,6 +96,7 @@ export default class PluginSample extends Plugin {
                 return;
             };
             targetId = targetNotebookId;
+            boxID = targetNotebookId;
         } else {
             // 不支持其他类型
             return;
@@ -108,6 +111,43 @@ export default class PluginSample extends Plugin {
                     fromIDs: [currentDoc.id],
                     toID: targetId,
                 });
+
+                // // TODO跟进: 没效果 https://github.com/siyuan-note/siyuan/issues/15617#issuecomment-3220161780
+                // const wait = await fetchSyncPost("/api/sqlite/flushTransaction");
+                // // console.log("wait", wait);
+
+                // // await new Promise(resolve => setTimeout(resolve, 1000));
+
+                // let sortJson = await this.getFile(`/data/${boxID}/.siyuan/sort.json`);
+                // // 解析 json，结构是 `{"20250814155701-4i5l68u":10,"20250822181321-ateqaxg":8}`，把 currentDoc.id 加到里面，并且索引值要比最低的那个 -1
+                // // 解析 sortJson，如果为空则初始化为一个对象
+                // let sortObj: Record<string, number>;
+                // try {
+                //     sortObj = typeof sortJson === "string" ? JSON.parse(sortJson) : (sortJson || {});
+                // } catch (e) {
+                //     // 如果解析失败，则初始化为空对象
+                //     sortObj = {};
+                // }
+
+                // // 获取当前所有索引的最小值
+                // let minIndex = 0;
+                // const values = Object.values(sortObj);
+                // if (values.length > 0) {
+                //     minIndex = Math.min(...values);
+                // }
+                // // 将 currentDoc.id 加入对象，索引值为 minIndex - 1
+                // // sortObj[currentDoc.id] = minIndex - 1;
+                // sortObj[currentDoc.id] = 100;
+
+                // // 赋值回 sortJson
+                // sortJson = sortObj;
+                // // console.log("sortJson", sortJson);
+                // // console.log("/data/${boxID}/.siyuan/sort.json", `/data/${boxID}/.siyuan/sort.json`);
+                // const response = await this.putFile(`/data/${boxID}/.siyuan/sort.json`, sortJson);
+                // // const response2 = await this.putFile(`/data/${boxID}/.siyuan/sort2.json`, sortJson);
+                // // console.log("response", response);
+                // // console.log("response2", response2);
+
                 if (this.data[STORAGE_NAME].expandDocTreeAfterMoveDoc) {
                     // 移动文档之后展开文档树 https://github.com/TCOTC/move-doc/issues/2
                     element.querySelector(".b3-list-item__toggle:has(svg.b3-list-item__arrow:not(.b3-list-item__arrow--open))")?.click();
@@ -127,4 +167,43 @@ export default class PluginSample extends Plugin {
             notebookId: protyle.notebookId
         };
     };
+
+    getFile(path: string): Promise<any> {
+        let data = {
+            path: path
+        }
+        return new Promise((resolve, _) => {
+            fetchPost("/api/file/getFile", data, (content: any) => {
+                resolve(content)
+            });
+        });
+    }
+
+    async putFile(path: string, json: any, isDir = false, modTime = Date.now()) {
+        let file;
+        if (typeof json === "object") {
+            file = new File(
+                [new Blob([JSON.stringify(json)], { type: "application/json" })],
+                path.split("/").pop()
+            );
+        } else {
+            file = new File([new Blob([json])], path.split("/").pop());
+        }
+    
+        let formdata = new FormData();
+        formdata.append("path", path);
+        formdata.append("file", file);
+        formdata.append("isDir", isDir.toString());
+        formdata.append("modTime", modTime.toString());
+    
+        const response = await fetch("/api/file/putFile", {
+            body: formdata,
+            method: "POST",
+        });
+    
+        if (response.ok)
+            return await response.json();
+        else
+            return null;
+    }
 }
