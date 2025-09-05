1015151c33b0694ac6262855d925c5cb8a63eb13
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2025-09-05 12:12:47 +0800

    WIP 移动的文档排序到目标位置的顶部
    https://github.com/TCOTC/move-doc/issues/3

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import {
     fetchSyncPost,
     getActiveEditor,
     expandDocTree,
+    showMessage,
 } from "siyuan";
 import "./index.scss";
 
@@ -63,16 +64,16 @@ export default class PluginSample extends Plugin {
             // 当前文档不存在
             return;
         }
-        let targetId: string;
 
+        let targetDocId: string;
+        let targetNotebookId: string;
         // doc: 单个文档；docs: 多个文档 / 文档与笔记本混合；notebook: 单个笔记本
         if (type === "doc") {
-            const targetDocId = element?.getAttribute('data-node-id');
+            targetDocId = element?.getAttribute('data-node-id');
             if (!targetDocId || currentDoc.path.slice(-48).includes(targetDocId)) {
                 // 排除当前文档、父文档
                 return;
             }
-            targetId = targetDocId;
             // TODO跟进: 目前不支持使用异步操作 https://github.com/siyuan-note/siyuan/issues/15676
             // const targetDocInfo = await fetchSyncPost("/api/block/getBlockInfo", { id: targetDocId });
             // if (!targetDocInfo?.data?.path || targetDocInfo.data.path.includes(currentDoc.id)) {
@@ -86,12 +87,11 @@ export default class PluginSample extends Plugin {
                 return;
             }
         } else if (type === "notebook") {
-            const targetNotebookId = element?.parentElement?.getAttribute('data-url');
+            targetNotebookId = element?.parentElement?.getAttribute('data-url');
             if (!targetNotebookId || (currentDoc.notebookId === targetNotebookId && currentDoc.path.length <= 26)) {
                 // 如果文档在笔记本根目录的话需要排除当前笔记本
                 return;
             }
-            targetId = targetNotebookId;
         } else {
             // 不支持其他类型
             return;
@@ -102,11 +102,56 @@ export default class PluginSample extends Plugin {
             icon: "iconMove",
             label: this.i18n[`moveToThis${type.charAt(0).toUpperCase() + type.slice(1).toLowerCase()}`],
             click: async () => {
-                await fetchSyncPost('/api/filetree/moveDocsByID', {
+                const res = await fetchSyncPost('/api/filetree/moveDocsByID', {
                     fromIDs: [currentDoc.id],
-                    toID: targetId,
+                    toID: targetDocId || targetNotebookId,
                 });
-                // 等待文档移动完成之后才能展开文档树
+                if (res.code !== 0) {
+                    // 移动文档失败
+                    showMessage(this.displayName + ": " + this.i18n.moveDocFailed, undefined, "error", "move-doc_move-doc-failed");
+                    return;
+                }
+                let notebook: string;
+                let path: string;
+                if (targetDocId) {
+                    const res2 = await fetchSyncPost('/api/filetree/getPathByID', {
+                        id: currentDoc.id,
+                    });
+                    console.log("res2", res2);
+                    notebook = res2.data?.notebook;
+                    path = res2.data?.path;
+                    if (res2.code !== 0 || !notebook || !path) {
+                        // 获取文档路径失败
+                        showMessage(this.displayName + ": " + this.i18n.getPathByIDFailed, undefined, "error", "move-doc_get-path-by-id-failed");
+                        return;
+                    }
+                    targetNotebookId = notebook;
+                }
+                const res3 = await fetchSyncPost('/api/filetree/listDocsByPath', {
+                    notebook: targetNotebookId,
+                    path: targetDocId || "/",
+                    maxListCount: 1, // 只需要获取最上面的文档的排序就够了
+                    showHidden: true, // 包含隐藏的文档
+                    ignoreMaxListHint: true, // 忽略最大数量提示
+                });
+                console.log("res3", res3);
+                const firstDocId = res3.data?.files[0]?.id;
+                const firstDocSort = res3.data?.files[0]?.sort;
+                if (res3.code !== 0 || !firstDocId || !firstDocSort) {
+                    // 通过路径获取文档失败
+                    showMessage(this.displayName + ": " + this.i18n.getDocsByPathFailed, undefined, "error", "move-doc_get-docs-by-path-failed");
+                    return;
+                }
+                if (firstDocId !== currentDoc.id) {
+                    // TODO: 修改排序，需要原生 API https://github.com/siyuan-note/siyuan/issues/15776
+                    // 这个 API 不能指定排序值：
+                    // const res4 = await fetchSyncPost("/api/filetree/changeSort", {
+                    //     paths: [path],      // 新的排序路径数组
+                    //     notebook: notebook  // 目标笔记本 ID
+                    // });
+                    // console.log("res4", res4);
+                }
+                // 等待文档移动并排序完成之后才能展开文档树
                 if (this.data[STORAGE_NAME].expandDocTreeAfterMoveDoc) {
                     // 移动文档之后展开文档树 https://github.com/TCOTC/move-doc/issues/2
                     expandDocTree({ id: currentDoc.id });
